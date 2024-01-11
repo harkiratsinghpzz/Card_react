4018e9814cc50238e96236b3c3e85630cf3cc8f8
harkirat <harkiratsingh76376@gmail.com>
2024-01-11 18:05:15 -0500

    cardcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import myproduct from './main.module.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Main() {
+    let [product,setproduct] = useState([])
+
+let getdata = async()=>{
+    let response = await axios
+    .get('https://fakestoreapi.com/products')
+    .then(val=>{
+        console.log(val.data);
+        setproduct(val.data)
+    })
+    .catch(err=>{
+        console.log(err);
+    }) 
+
 }
 
-export default App;
+useEffect(()=>{
+    getdata()
+},[])
+
+// render data
+
+let renderdata = product.map((ele,index)=>(
+    <div key={index} className={myproduct.card}>
+        <div className="card-title">
+            <h3>{ele.title}</h3>
+        </div>
+        <div className="card-body">
+            <img className='img-fluid' src={ele.image} alt="" />
+            <p>{ele.description}</p>
+        </div>
+        <div className="card-footer">
+            <h1> ${ele.price}</h1>
+        </div>
+    </div>
+))
+
+  return (
+    <div className={myproduct.container}>
+{
+    renderdata
+}
+    </div>
+  )
+}
+
+export default Main
+
+
+// 1) send request to get the data
+// 2) updatae a state with incoming data
+// render the data
